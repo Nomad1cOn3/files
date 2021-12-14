d91146bc6f4ba92a44cc398bd08a0df98bfdddba
Nomad1cOn3 <bostroff2@gmail.com>
2021-12-14 13:58:22 +0000

    New File Created

---
+++ sorting-function-test.py
@@ -0,0 +1,12 @@
+list1 = []
+list2 = []
+l = None
+for x in range(1,10):
+    try:
+        l = input()
+        l = int(l)
+        list1.append(l)
+    except:
+        list2.append(l)
+print(sorted(list1))
+print(sorted(list2))
