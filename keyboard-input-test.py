bb079e05cf5dbcc1c4907eb30edabedddfe77d90
Nomad1cOn3 <bostroff2@gmail.com>
2021-12-14 14:06:54 +0000

    Created New File

---
+++ keyboard-input-test.py
@@ -0,0 +1,21 @@
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+getch = _GetchUnix()
+print ("Please enter something: ")
+l = getch()
+for x in range(1,10):
+    print(l)
